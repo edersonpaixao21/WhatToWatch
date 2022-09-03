a64eaa26db4a235e87337bef438aa5df05b707e8
Juan Ferreira <juan.ferreira@impar.com.br>
2022-09-02 22:01:56 -0300

    :fix: erro endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
+import {  useState } from 'react'
 import { api, apiKey, apiUrlImg, language } from './Api/api'
 
 interface FilmProps {
   id: number;
   title: string;
-  description: string;
-  poster: string;
+  description?: string;
+  poster?: string;
+  overview?: string;
 }
 
 export function App() {
   const [film, setFilm] =useState<FilmProps>({} as FilmProps)
 
-  function handleSearchFilm() {
-      api.get(`${api}${apiKey}`)
-      .then(res =>{
-        const data = res.data.results [Math.floor(Math.random() )];
 
-        const imgPoster = `${apiUrlImg}${data.poster_path}`;
+  const handleSearchFilm = () =>{
+       api.get(`${apiKey}`)
+      .then((res: any) =>{
+       const data: FilmProps = res.data;
 
+        const imgPoster = `${apiUrlImg}${res.data.poster_path}`;
+
+      //  const imgPoster = `${apiUrlImg}${data.poster_path}`;
         const film = {
           id: data.id,
           title:data.title,
           description: data.overview,
           poster:imgPoster
         }
-
+        console.log(imgPoster)
         setFilm(film);  
       })
       
@@ -45,7 +47,7 @@ export function App() {
     <main>
 
       <div>
-        <img src="" alt="" />
+        <img src={film.poster} alt="" />
         <h1>Não sabe o que assistir?</h1>
       </div>
 
